28b9427418f69c451db6dc92f8c63ea4befa84a9
sahidul-newroz <sahidul@newroztech.com>
2024-10-08 13:01:23 +0600

    Update 1.2.1.

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
            name: "UserVerification",
            url: "https://github.com/FinioTech/UserVerification-iOS-SDK/releases/download/1.2.1/UserVerification.xcframework.zip",
-           checksum: "1d375185d3d1ae4003e3d48d54b7f1d4fcb565f089644036da1b29adea366565"
+           checksum: "ca398096b4357ce5dd90a9b331d82b228a254f53b0a9e43acd33015ec2656a8b"
         )
     ],
     swiftLanguageVersions: [.v5]
